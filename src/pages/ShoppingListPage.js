2dabcddbe207111938dc5aa23ebf53f184805cdc
Mahesh Yadav <mahesh.iitmandi@gmail.com>
2021-09-16 17:40:31 +0530

    add shopping list page

--- src/pages/ShoppingListPage.js
+++ src/pages/ShoppingListPage.js
@@ -1,5 +1,29 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { useFetch } from '../hooks/useFetch';
 
 export default function ShoppingListPage() {
-  return <div>ShoppingListPage</div>;
+  const history = useHistory();
+
+  const { isLoading, data: shoppingItems } = useFetch('/shopping-items');
+
+  return (
+    <div className="page">
+      <button onClick={() => history.goBack()} className="back-button">
+        Back
+      </button>
+      <div className="centered-container">
+        <h1>Here's your shopping list:</h1>
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <>
+            {shoppingItems.map((item) => (
+              <p key={item}>{item}</p>
+            ))}
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
